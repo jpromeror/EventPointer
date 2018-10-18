bf4ff11d232bd33e81cf9c80704876886ac712d3
Ferrer-Bonsoms Hernandez <jafhernandez@tecnun.es>
2018-10-18 09:35:16 +0200

    jaf: read .h5 files

--- R/getbootstrapkallisto.R
+++ R/getbootstrapkallisto.R
@@ -20,11 +20,20 @@
 #'
 #'
 #' @export
-#' @importFrom sleuth read_kallisto_h5
+#' @importFrom rhdf5 h5read 
 
 
 
-getbootstrapkallisto <- function(pathValues=NA,nb=0){
+getbootstrapkallisto <- function(pathValues=NA,nb){
+  
+  if(is.null(nb)){
+    stop("nb field is empty")
+  }
+  
+  if(nb == 0){
+    stop("nb must be >0")
+  }
+  
   
   if(is.na(pathValues)){
     stop("not pathValues")
@@ -37,29 +46,59 @@ getbootstrapkallisto <- function(pathValues=NA,nb=0){
     name <- unlist(strsplit(pathValues[jj],"/"))
     name <- name[length(name)]
     
-    bootsdata <- read_kallisto_h5(paste0(pathValues[jj],"/abundance.h5"),read_bootstrap = TRUE,max_bootstrap = nb)
-    if (jj ==1){ #en la primera iteracion inicializar la matriz mymatrix y ponerle el rowname
-      mymatrix <- matrix(0,nrow=nrow(bootsdata$abundance),ncol = (nb+1)) # solo inicializar la matrix 1 vez
-      rownames(mymatrix) <- bootsdata$abundance$target_id
+    annotationreference <- h5read(paste0(pathValues[jj],"/abundance.h5"),"aux/ids")
+    
+    llkb <- h5read(paste0(pathValues[jj],"/abundance.h5"),"aux/eff_lengths")
+    abund <- h5read(paste0(pathValues[jj],"/abundance.h5"),"/est_counts")
+    
+    kkk <- abund/llkb
+    ppx <- which(llkb==0)
+    kkk[ppx]  <- 0
+    abund_tpm <- 1/sum(kkk)*kkk*1e6
+    
+    #read bootstrap data:
+    
+    for (iix in 0:(nb-1)){
+      command <- paste0("abund_b",iix,
+                        " <- h5read(paste0(pathValues[jj],'/abundance.h5'),'bootstrap/bs",iix,"')")
+      #print(command)
+      eval(parse(text = command))
+      
+      
+      command <- paste0("kkk <- abund_b",iix,"/llkb")
+      #print(command)
+      eval(parse(text = command))
+      
+      kkk[ppx] <- 0
+      
+      
+      command <- paste0("abund_b",iix,"_tpm <- 1/sum(kkk)*kkk*1e6")
+      #print(command)
+      eval(parse(text = command))
+    }
+    command <- paste0("losboots <- cbind(",paste(paste0("abund_b",0:19,"_tpm"),
+                                                 collapse = ","),")")
+    eval(parse(text = command))
+    
+    #add the value of the m.l (bootstrap = 0)
+    if (jj ==1){ 
+      #en la primera iteracion inicializar la matriz mymatrix y ponerle el rowname 
+      #(el resto tendran que tener el mismo orden)
+      mymatrix <- matrix(0,nrow=nrow(abund_tpm),ncol = (nb+1)) # solo inicializar la matrix 1 vez
+      rownames(mymatrix) <- annotationreference
       #en la primera columna poner el abundance (bootstrap 0)
-      mymatrix[,1] <- bootsdata$abundance$tpm
-    }else{ #poner el abundance cuando ya no es la primera iteracion, comprobar que estan en el mismo orden
-      if(identical(rownames(mymatrix),bootsdata$abundance$target_id)){
-        mymatrix[,1] <- bootsdata$abundance$tpm
+      mymatrix[,1] <- abund_tpm
+      #En el resto de columnas estan los bootstrap
+      mymatrix[,2:(nb+1)] <- losboots
+    }else{ #comprobar que estan en el mismo orden para el resto de muestras
+      if(identical(rownames(mymatrix),annotationreference)){
+        mymatrix[,1] <- abund_tpm
+        mymatrix[,2:(nb+1)] <- losboots
       }else{ #comprobar que estan en el mismo orden
-        mymatrix[,1] <- bootsdata$abundance$tpm[match(rownames(mymatrix),bootsdata$abundance$target_id)]
+        mymatrix[,1] <- abund_tpm[match(rownames(mymatrix),annotationreference)]
+        mymatrix[,2:(nb+1)] <- losboots[match(rownames(mymatrix),annotationreference)]
       }
     }
-    #en cualquier iteracion hay q comprobar el orden de los datos del bootstrap
-    
-    mymatrix[,2:(nb+1)] <- sapply(bootsdata$bootstrap,function(X){
-      if(identical(X$target_id,rownames(mymatrix))){
-        return(X$tpm)
-      }else{
-        return(X$tpm[match(rownames(mymatrix),X$target_id)])
-      }
-    })
-    
     
     datafinal[[jj]]<-mymatrix
     names(datafinal)[jj]<-name
--- inst/unitTests/test_getbootstrapkallisto.R
+++ inst/unitTests/test_getbootstrapkallisto.R
@@ -1,9 +1,17 @@
 test_getbootstrapkallisto <- function() {
   
   obs <- tryCatch(getbootstrapkallisto(pathValues=NA,
-                                       nb=0), error=conditionMessage)
-  
-  
+                                       nb=20), error=conditionMessage)
   
   checkIdentical("not pathValues", obs)
+  
+  obs <- tryCatch(getbootstrapkallisto(pathValues="text",
+                                       nb=NULL), error=conditionMessage)
+  
+  checkIdentical("nb field is empty", obs)
+  
+  obs <- tryCatch(getbootstrapkallisto(pathValues="text",
+                                       nb=0), error=conditionMessage)
+  
+  checkIdentical("nb must be >0", obs)
 }
